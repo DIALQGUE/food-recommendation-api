746efbc9f9fafd79ec5ff77d807fa763d1ad8058
DIALQGUE <fang_za2000@hotmail.co.th>
2022-06-29 10:28:36 +0700

    refactor: :recycle: replace dialogflow-fulfillment with self-implement workflow
    remove dialogflow-fulfillment and handlebars

--- routes/api/fulfillment.js
+++ routes/api/fulfillment.js
@@ -1,27 +1,113 @@
 const express = require('express');
-const Foods = require('../../models/foods-model');
+const { Foods, UserHistory } = require('../../models/foods-model');
 const router = express.Router();
 
-const { WebhookClient } = require('dialogflow-fulfillment');
+//const { WebhookClient } = require('dialogflow-fulfillment');
+
+const fulfillmentMessages = {
+    "fulfillmentMessages": [
+        {
+            "text": {
+                "text": [
+                    "Default webhook response"
+                ]
+            }
+        }
+    ]
+}
+
+var lastSuggestion = "";
 
 router.post('/', function (req, res) {
-    const agent = new WebhookClient({
-        request: req,
-        response: res
-    });
+    // const agent = new WebhookClient({
+    //     request: req,
+    //     response: res
+    // });
 
-    let intentMap = new Map();
+    // let intentMap = new Map();
 
-    intentMap.set('Recommend - condition - yes', (agent) => {
+    // intentMap.set('Recommend - condition - yes', (agent) => {
+    //     return new Promise((resolve, reject) => {
+    //         Foods.findOne({ 'tag.ingredient': agent.parameters.condition }).lean().exec((err, found) => {
+    //             if (!found)
+    //                 resolve('ไม่มีอาหารที่มีคุณลักษณะนี้ในระบบ');
+    //             else {
+    //                 Foods.find({ 'tag.ingredient': agent.parameters.condition }).lean().exec((err, selectedFoods) => {
+    //                     try {
+    //                         var random = Math.floor(Math.random() * selectedFoods.length);
+    //                         const food = selectedFoods[random];
+    //                         resolve(`${food.name}`);
+    //                     }
+    //                     catch (err) {
+    //                         reject(err);
+    //                     }
+    //                 });
+    //             }
+    //         })
+    //     })
+    //         .then((msg) => {
+    //             agent.setContext({ name: 'response', lifespan: 1, parameters: { food: msg } });
+    //         })
+    //         .catch(err => {
+    //             console.log(err);
+    //         });
+    // });
+
+
+    // intentMap.set('Recommend - no condition', (agent) => {
+    //     return new Promise((resolve, reject) => {
+    //         Foods.find().lean().exec((err, foods) => {
+    //             try {
+    //                 var random = Math.floor(Math.random() * foods.length);
+    //                 const food = foods[random];
+    //                 console.log(`randomized food: ${food.name}`);
+    //                 resolve(`${food.name}`);
+    //             }
+    //             catch (err) {
+    //                 reject(err);
+    //             }
+    //         });
+    //     })
+    //         .then((msg) => {
+    //             agent.setContext({ name: 'response', lifespan: 1, parameters: { food: msg } });
+    //         })
+    //         .catch(err => {
+    //             console.log(err);
+    //         });
+    // });
+
+    // intentMap.set('Recommend - accepted', (agent) => {
+    //     const history = new userHistory({
+    //         user_id: agent.originalRequest.payload.data.user.id,
+    //         food: agent.getContext('response').parameters.food,
+    //         date: Date.now(),
+    //         accepted: true
+    //     });
+
+    //     history.save((err, saved) => {
+    //         if (err)
+    //             console.log(err);
+    //         else
+    //             console.log(`user history saved: ${saved}`);
+    //     });
+    // });
+
+    // agent.handleRequest(intentMap);
+
+    const intent = req.body.queryResult.intent.displayName;
+    const parameters = req.body.queryResult.parameters;
+
+    if (intent === 'Recommend - condition - yes') {
         return new Promise((resolve, reject) => {
-            Foods.findOne({ 'tag.ingredient': agent.parameters.condition }).lean().exec((err, found) => {
+            Foods.findOne({ 'tag.ingredient': parameters.condition }).lean().exec((err, found) => {
                 if (!found)
                     resolve('ไม่มีอาหารที่มีคุณลักษณะนี้ในระบบ');
                 else {
-                    Foods.find({ 'tag.ingredient': agent.parameters.condition }).lean().exec((err, selectedFoods) => {
+                    Foods.find({ 'tag.ingredient': parameters.condition }).lean().exec((err, selectedFoods) => {
                         try {
                             var random = Math.floor(Math.random() * selectedFoods.length);
                             const food = selectedFoods[random];
+                            console.log(`randomized food: ${food.name}`);
                             resolve(`${food.name}`);
                         }
                         catch (err) {
@@ -32,15 +118,17 @@ router.post('/', function (req, res) {
             })
         })
             .then((msg) => {
-                agent.setContext({ name: 'response', lifespan: 1, parameters: { food: msg } });
+                lastSuggestion = msg;
+                let newResponse = fulfillmentMessages;
+                newResponse.fulfillmentMessages[0].text.text[0] = `เราขอแนะนำเมนู ${msg}`;
+                res.send(newResponse);
             })
             .catch(err => {
                 console.log(err);
             });
-    });
+    }
 
-
-    intentMap.set('Recommend - no condition', (agent) => {
+    else if (intent === 'Recommend - no condition') {
         return new Promise((resolve, reject) => {
             Foods.find().lean().exec((err, foods) => {
                 try {
@@ -55,30 +143,34 @@ router.post('/', function (req, res) {
             });
         })
             .then((msg) => {
-                agent.setContext({ name: 'response', lifespan: 1, parameters: { food: msg } });
+                lastSuggestion = msg;
+                let newResponse = fulfillmentMessages;
+                newResponse.fulfillmentMessages[0].text.text[0] = `เราขอแนะนำเมนู ${msg}`;
+                res.send(newResponse);
             })
             .catch(err => {
                 console.log(err);
             });
-    });
+    }
 
-    intentMap.set('Recommend - accepted', (agent) => {
+    else if (intent === 'Recommend - accepted') {
         const history = new userHistory({
-            user_id: agent.originalRequest.payload.data.user.id,
-            food: agent.getContext('response').parameters.food,
+            user_id: req.body.originalDetectIntentRequest.payload.data.user.id,
+            food: lastSuggestion,
             date: Date.now(),
             accepted: true
         });
 
-        history.save((err, saved) => {
-            if (err)
-                console.log(err);
-            else
-                console.log(`user history saved: ${saved}`);
-        });
-    });
-
-    agent.handleRequest(intentMap);
+        if (lastSuggestion) {
+            history.save((err, saved) => {
+                if (err)
+                    console.log(err);
+                else
+                    console.log(`user history saved: ${saved}`);
+            });
+        }
+        lastSuggestion = '';
+    }
 });
 
 module.exports = router;
