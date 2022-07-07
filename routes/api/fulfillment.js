3af64a6d28d763cb019c51c0acfe852a27e266af
DIALQGUE <fang_za2000@hotmail.co.th>
2022-07-07 16:04:47 +0700

    test: :white_check_mark: try new type of line response

--- routes/api/fulfillment.js
+++ routes/api/fulfillment.js
@@ -3,8 +3,6 @@ const router = express.Router();
 const { Foods, UserHistory } = require('../../models/foods-model');
 const recommend = require('../../recommend');
 
-//const { WebhookClient } = require('dialogflow-fulfillment');
-
 function getContextPrefix(outputContexts) {
     if (outputContexts.length > 0) {
         return outputContexts[0].name.split('/').slice(0, -1).join('/');
@@ -12,94 +10,22 @@ function getContextPrefix(outputContexts) {
     return '';
 }
 
-const fulfillmentMessages = {
+const fulfillmentResponse = {
     "fulfillmentMessages": [
         {
             "text": {
-                "text": [
-                    "Default webhook response"
-                ]
+                "text": []
             }
         }
     ],
-    "outputContexts": []
+    "outputContexts": [],
+    "payload": {
+        "line": {
+        }
+    }
 }
 
 router.post('/', function (req, res) {
-    // const agent = new WebhookClient({
-    //     request: req,
-    //     response: res
-    // });
-
-    // let intentMap = new Map();
-
-    // intentMap.set('Recommend - condition - yes', (agent) => {
-    //     return new Promise((resolve, reject) => {
-    //         Foods.findOne({ 'tag.ingredient': agent.parameters.condition }).lean().exec((err, found) => {
-    //             if (!found)
-    //                 resolve('ไม่มีอาหารที่มีคุณลักษณะนี้ในระบบ');
-    //             else {
-    //                 Foods.find({ 'tag.ingredient': agent.parameters.condition }).lean().exec((err, selectedFoods) => {
-    //                     try {
-    //                         var random = Math.floor(Math.random() * selectedFoods.length);
-    //                         const food = selectedFoods[random];
-    //                         resolve(`${food.name}`);
-    //                     }
-    //                     catch (err) {
-    //                         reject(err);
-    //                     }
-    //                 });
-    //             }
-    //         })
-    //     })
-    //         .then((msg) => {
-    //             agent.setContext({ name: 'response', lifespan: 1, parameters: { food: msg } });
-    //         })
-    //         .catch(err => {
-    //             console.log(err);
-    //         });
-    // });
-
-
-    // intentMap.set('Recommend - no condition', (agent) => {
-    //     return new Promise((resolve, reject) => {
-    //         Foods.find().lean().exec((err, foods) => {
-    //             try {
-    //                 var random = Math.floor(Math.random() * foods.length);
-    //                 const food = foods[random];
-    //                 console.log(`randomized food: ${food.name}`);
-    //                 resolve(`${food.name}`);
-    //             }
-    //             catch (err) {
-    //                 reject(err);
-    //             }
-    //         });
-    //     })
-    //         .then((msg) => {
-    //             agent.setContext({ name: 'response', lifespan: 1, parameters: { food: msg } });
-    //         })
-    //         .catch(err => {
-    //             console.log(err);
-    //         });
-    // });
-
-    // intentMap.set('Recommend - accepted', (agent) => {
-    //     const history = new userHistory({
-    //         user_id: agent.originalRequest.payload.data.user.id,
-    //         food: agent.getContext('response').parameters.food,
-    //         date: Date.now(),
-    //         accepted: true
-    //     });
-
-    //     history.save((err, saved) => {
-    //         if (err)
-    //             console.log(err);
-    //         else
-    //             console.log(`user history saved: ${saved}`);
-    //     });
-    // });
-
-    // agent.handleRequest(intentMap);
 
     const intent = req.body.queryResult.intent.displayName;
     const parameters = req.body.queryResult.parameters;
@@ -151,9 +77,30 @@ router.post('/', function (req, res) {
             })
         })
             .then((msg) => {
-                let newResponse = fulfillmentMessages;
+                let newResponse = fulfillmentResponse;
                 if (success) {
-                    newResponse.fulfillmentMessages[0].text.text[0] = `เราขอแนะนำเมนู ${msg}\nเมนูนี้ถูกใจคุณรึเปล่า`;
+                    //newResponse.fulfillmentMessages[0].text.text[0] = `เราขอแนะนำเมนู ${msg}\nเมนูนี้ถูกใจคุณรึเปล่า`;
+
+                    newResponse.payload.line = {
+                        type: "template",
+                        altText: `เราขอแนะนำเมนู ${msg}\nเมนูนี้ถูกใจคุณรึเปล่า`,
+                        template: {
+                            type: "confirm",
+                            text: `เราขอแนะนำเมนู ${msg}`,
+                            actions: [
+                                {
+                                    "type": "message",
+                                    "label": "ถูกใจ",
+                                    "text": "ถูกใจ"
+                                },
+                                {
+                                    "type": "message",
+                                    "label": "ไม่ถูกใจ",
+                                    "text": "ไม่ถูกใจ"
+                                }
+                            ]
+                        }
+                    }
 
                     newResponse.outputContexts = req.body.queryResult.outputContexts;
                     const contextPrefix = getContextPrefix(newResponse.outputContexts);
@@ -196,8 +143,29 @@ router.post('/', function (req, res) {
             });
         })
             .then((msg) => {
-                let newResponse = fulfillmentMessages;
-                newResponse.fulfillmentMessages[0].text.text[0] = `เราขอแนะนำเมนู ${msg}\nเมนูนี้ถูกใจคุณรึเปล่า`;
+                let newResponse = fulfillmentResponse;
+                //newResponse.fulfillmentMessages[0].text.text[0] = `เราขอแนะนำเมนู ${msg}\nเมนูนี้ถูกใจคุณรึเปล่า`;
+
+                newResponse.payload.line = {
+                    type: "template",
+                    altText: `เราขอแนะนำเมนู ${msg}\nเมนูนี้ถูกใจคุณรึเปล่า`,
+                    template: {
+                        type: "confirm",
+                        text: `เราขอแนะนำเมนู ${msg}`,
+                        actions: [
+                            {
+                                "type": "message",
+                                "label": "ถูกใจ",
+                                "text": "ถูกใจ"
+                            },
+                            {
+                                "type": "message",
+                                "label": "ไม่ถูกใจ",
+                                "text": "ไม่ถูกใจ"
+                            }
+                        ]
+                    }
+                }
 
                 newResponse.outputContexts = req.body.queryResult.outputContexts;
                 const contextPrefix = getContextPrefix(newResponse.outputContexts);
